0fa85463b69b50d3912b0976e05dc07914e8bf2e
realtoastyboio <93612029+realtoastyboio@users.noreply.github.com>
2021-12-06 16:15:07 +0200

    Create mcirowave.py

---
+++ mcirowave.py
@@ -0,0 +1,8 @@
+input == input;
+
+if input == "beep":
+  while 3 > 2:
+    print("MMMMMMMMMMMMMMMMMMMMMMMMMMMM BEEEP MMMMMMMMMMMMMM BEEEP MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM")
+    
+else:
+  print(" bro imagine not being microvawe ")
